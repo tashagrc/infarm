3b5babeb9ef297c8efde2cab27115c4790c35d9f
lixkelik <lixkelik@gmail.com>
2022-12-15 12:18:06 +0700

    update

---
+++ lib/constants/constantBuilder.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+export 'package:velocity_x/velocity_x.dart';
+
+//COLORS
+const Color bgWhite = Color(0xffF1F1F1);
+const Color textfield = Color(0xffEBEBEB);
+const Color grey = Color(0xffA0A0A0);
+const Color appBlue = Color(0xff4D4672);
+const Color appYellow = Color(0xffF9B060);
+
+//IMAGES
+const landingBg = 'assets/images/landing_page_bg.png';
+const loginArt = 'assets/images/login_art.png';
+const loginBg = 'assets/images/login_bg.png';
+
+//LOGO
+const cartIcon = 'assets/icons/cart.png';
+const cartBg = 'assets/icons/cart_empty.png';
+const historyIcon = 'assets/icons/history.png';
+const homeIcon = 'assets/icons/home.png';
+const profileIcon = 'assets/icons/profile.png';
+const forgetPassIcon = 'asssets/icons/forget_pass.png';
+
+// FONT
+const regular = "OpenSans-Regular";
+const semiBold = "OpenSans-SemiBold";
+const bold = "OpenSans-Bold";
