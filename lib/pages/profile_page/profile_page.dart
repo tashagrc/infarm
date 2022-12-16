3033e8887852e8df6715e458bfa9429fab633186
lixkelik <lixkelik@gmail.com>
2022-12-17 01:57:02 +0700

    update scroll physics

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -88,6 +88,7 @@ class ProfilePage extends StatelessWidget {
                   ),
                   25.heightBox,
                   ListView.separated(
+                    physics: const NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     separatorBuilder: (context, index) {
                       return const Divider(color: grey,);
