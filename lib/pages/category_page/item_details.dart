464537cd41523530db2eb49b74b45f19b51d0348
Natasha Grace <natashagradika@gmail.com>
2022-12-18 09:44:42 +0700

    item detail done

--- lib/pages/category_page/item_details.dart
+++ lib/pages/category_page/item_details.dart
@@ -7,47 +7,47 @@ class ItemDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: lightGrey,
-        appBar: AppBar(
-            title: title!.text.color(darkGrey).fontFamily(bold).make(),
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.share),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.favorite_outline),
-              ),
-            ]),
-        body: Column(children: [
+      backgroundColor: lightGrey,
+      appBar: AppBar(
+          title: title!.text.color(darkGrey).fontFamily(bold).make(),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.share),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.favorite_outline),
+            ),
+          ]),
+      body: Column(
+        children: [
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
+            child: Padding(
                 padding: const EdgeInsets.all(8),
                 child: SingleChildScrollView(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      // swipe2
+                      // swipe2 section
                       VxSwiper.builder(
                           autoPlay: true,
                           height: 350,
                           itemCount: 3,
+                          aspectRatio: 16 / 9,
                           itemBuilder: (context, index) {
                             return Image.asset(product1,
                                 width: double.infinity, fit: BoxFit.cover);
                           }),
 
-                      10.heightBox,
-
-                      // title + detail
+                      // title n detail
                       title!.text
                           .size(16)
                           .color(darkGrey)
                           .fontFamily(semiBold)
                           .make(),
+
+                      // rating
                       10.heightBox,
                       VxRating(
                           onRatingUpdate: (value) {},
@@ -57,6 +57,7 @@ class ItemDetails extends StatelessWidget {
                           count: 5,
                           size: 25,
                           stepInt: true),
+
                       10.heightBox,
                       "Rp24.000"
                           .text
@@ -66,31 +67,163 @@ class ItemDetails extends StatelessWidget {
                           .make(),
 
                       10.heightBox,
+                      // seller
                       Row(
                         children: [
                           Expanded(
                               child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                "Seller".text.white.fontFamily(semiBold).make(),
-                                5.heightBox,
-                                "Brands"
-                                    .text
-                                    .fontFamily(semiBold)
-                                    .color(darkGrey)
-                                    .size(16)
-                                    .make()
-                              ])),
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              "Seller".text.make(),
+                              5.heightBox,
+                              "Brands"
+                                  .text
+                                  .fontFamily(semiBold)
+                                  .color(darkGrey)
+                                  .size(16)
+                                  .make()
+                            ],
+                          )),
                           const CircleAvatar(
                             backgroundColor: Colors.white,
                             child: Icon(Icons.message_rounded, color: darkGrey),
-                          )
+                          ),
                         ],
-                      ).box.height(70).color(grey).make(),
+                      )
+                          .box
+                          .height(60)
+                          .padding(const EdgeInsets.symmetric(horizontal: 16))
+                          .color(grey)
+                          .make(),
+
+                      // quantity section
+                      20.heightBox,
+                      Column(
+                        // quantity
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                child: "Quantity".text.color(grey).make(),
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.remove)),
+                                  "0"
+                                      .text
+                                      .size(16)
+                                      .color(darkGrey)
+                                      .fontFamily(bold)
+                                      .make(),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.add)),
+                                  10.widthBox,
+                                  "(0 available)".text.color(grey).make(),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                          // price
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                child: "Total price".text.color(grey).make(),
+                              ),
+                              "Rp0"
+                                  .text
+                                  .color(Colors.red)
+                                  .size(16)
+                                  .fontFamily(bold)
+                                  .make(),
+                            ],
+                          ),
+                        ],
+                      ),
+
+                      // description section
+                      10.heightBox,
+
+                      "Description"
+                          .text
+                          .color(darkGrey)
+                          .fontFamily(semiBold)
+                          .make(),
+
+                      "This is a dummy item desc aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                          .text
+                          .color(darkGrey)
+                          .make(),
+
+                      // product you may also like
+                      "Product you may also like"
+                          .text
+                          .fontFamily(bold)
+                          .size(16)
+                          .color(darkGrey)
+                          .make(),
+                      SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: List.generate(
+                              6,
+                              (index) => Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius: const BorderRadius.only(
+                                            topLeft: Radius.circular(7),
+                                            topRight: Radius.circular(7)),
+                                        child: Image.asset(
+                                          product1,
+                                          width: 150,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      10.heightBox,
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 8),
+                                        child: "Pestisida Pesnab"
+                                            .text
+                                            .fontFamily(semiBold)
+                                            .color(darkGrey)
+                                            .make(),
+                                      ),
+                                      7.heightBox,
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 8),
+                                        child: "Rp24.000"
+                                            .text
+                                            .fontFamily(bold)
+                                            .color(appYellow)
+                                            .size(16)
+                                            .make(),
+                                      ),
+                                      7.heightBox
+                                    ],
+                                  )
+                                      .box
+                                      .white
+                                      .roundedSM
+                                      .margin(const EdgeInsets.symmetric(
+                                          horizontal: 4))
+                                      .make()),
+                        ),
+                      ),
                     ],
                   ),
                 )),
-          )),
+          ),
           SizedBox(
               width: double.infinity,
               height: 60,
@@ -99,6 +232,8 @@ class ItemDetails extends StatelessWidget {
                   onPress: () {},
                   textColor: white,
                   text: "Add to cart"))
-        ]));
+        ],
+      ),
+    );
   }
 }
